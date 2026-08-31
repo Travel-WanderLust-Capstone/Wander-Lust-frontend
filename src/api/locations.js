f93df66f0f8f3a6dcd55218c98d9fba8ca14c039
cancar5 <casey.ancar05@gmail.com>
2026-08-31 01:09:50 -0500

    Update API URL for fetching locations

--- src/api/locations.js
+++ src/api/locations.js
@@ -1,9 +1,9 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
-const API_URL = `${BASE_URL}/location`;
+
 
 export async function getLocations() {
   try {
-    const response = await fetch(API_URL);
+    const response = await fetch(`${BASE_URL}/locations`);
 
     if (!response.ok) {
       throw new Error("Failed to fetch locations");
