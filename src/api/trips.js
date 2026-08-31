ff5454afabdac79b13f11ca70baf3f06bdfe7f87
Asher Pardel <gracepardel7@gmail.com>
2026-08-31 00:55:24 -0400

    Update BASE_URL to use environment variable

--- src/api/trips.js
+++ src/api/trips.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API;
 const API_URL = `${BASE_URL}/trips`;
 
 //async tells JavaaScript function will perform something that takes time
