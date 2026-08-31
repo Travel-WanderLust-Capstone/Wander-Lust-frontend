4ef70b33808d54b0a2b9cfdf720a35c473c13d2e
Asher Pardel <gracepardel7@gmail.com>
2026-08-31 00:58:39 -0400

    Update API URL to use environment variable

--- src/explore/explore.jsx
+++ src/explore/explore.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router";
-const API = "https://wander-lust-oexs.onrender.com";
+const API = import.meta.env.VITE_API_URL;
 
 export default function Explore() {
   const [locations, setLocations] = useState([]);
